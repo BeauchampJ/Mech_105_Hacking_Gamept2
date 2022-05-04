d27df2651e99735614803528117b03515ed9ab44
Jacob Beauchamp <74421153+BeauchampJ@users.noreply.github.com>
2022-05-04 12:41:33 -0600

    Update killSwitch.sh

--- room2/room3/killSwitch.sh
+++ room2/room3/killSwitch.sh
@@ -6,13 +6,15 @@ then
 echo "Incorrect wire cut. Now launching virus!"
 echo "Countdown started"
 say "Countdown Started"
-echo "5"
-echo "4"
-echo "3"
-echo "2"
-echo "1"
-echo "Payload failed to launch, try cutting a different wire"
+say "5"
+say "4"
+say "3"
+say "2"
+say "1"
+echo "Hmm. Guess nothing happened. Payload failed to launch, try cutting a different wire"
 else
 echo "Good job Agent Wilson! You saved the world and beat the game! The true killswitch was 
 the friends we made along the way"
+echo "FOR BEATING THE GAME: FOR THE EXTRA CREDIT QUESTION ON THE EXAM, THE PASSPHRASE IS 'SCHRODINGERS CAT IS DEAD'"
+
 fi
